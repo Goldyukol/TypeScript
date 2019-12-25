15741b63eddcf978d3a5fc3d75ec273a5a9b3a90
Yury <goldyukol12@gmail.com>
2019-12-25 15:37:11 +0300

    feat: add todolists

--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import axios from 'axios'
 
-interface IState {
-    todos: any
+
+
+interface ITodo {
+    id: string
+    title: string
+    addedDate: string
+    order: number
 }
 
+interface IState {
+    todos: ITodo[]
+}
 class Todos extends React.Component<{}, IState> {
 
-    state = {
-        todos: ''
+    state: IState = {
+        todos: []
     }
 
     componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/todo-lists', { withCredentials: true })
             .then(res => {
-                alert(res.data)
+                console.log(res.data)
+                this.setState({
+                    todos: res.data
+                })
             })
     }
     render() {
         return (
-            <div>{this.state.todos}
-            </div>
+            <div>{this.state.todos.map((el: ITodo) => { return <div>{el.id}, {el.title}, {el.order}, {el.addedDate}</div> })}</div>
         )
     }
 }
